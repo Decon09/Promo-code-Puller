6605a6f3a9fd5d5bb3e925a6b2ff9f1f0b2285c1
Decon09 <127850441+Decon09@users.noreply.github.com>
2023-03-14 17:02:36 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import xboxlivepy,requests
+
+
+def get_auth():
+    xbox_auth  = xboxlivepy.XboxLiveAuth ( 'email' ,  'password' )
+    return xbox_auth
+
+def pull_code(email, passw):
+    headers = {'Authorization': get_auth(email, passw)}
+    code = requests.post('https://profile.gamepass.com/v2/offers/47D97C390AAE4D2CA336D2F7C13BA074', headers=headers, timeout=None)
+    return code.json()['resource']
+
+with open('accounts.txt', 'r') as f:
+    lines = f.readlines()
+    for line in lines:
+        l = line.strip().split('|')
+        email = l[0]    
+        password = l[1]
+        pull_code(email, password)
+    
